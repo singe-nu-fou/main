adce37b7ee18e95485605eac0b0d9ec1b7348985
palmeste <palmeste@OAK-2UA3111NTY.amwins.local>
2015-05-04 09:43:33 -0500

    Added some responsiveness to the quick links visibility and the quick links descriptor.

--- views/portfolio.php
+++ views/portfolio.php
@@ -9,9 +9,9 @@
 </script>
 <div class="row" style="padding-top:15px;">
     <div class="col-lg-8" style="height:10000px;">
-        <p style="text-align:justify;">As I am primarily a language agnostic web developer, I use a variety of software languages to attain the results that the current job calls for. Below I will post content that I have created in my personal endeavors that I feel show great example of my skills through various programming languages from PHP to Java, JavaScript to Angular. These will showcase the various development skills I have built over time and include links to github repositories. To the right is a navigation panel for the code snippets below.</p>
+        <p style="text-align:justify;">As I am primarily a language agnostic web developer, I use a variety of software languages to attain the results that the current job calls for. Below I will post content that I have created in my personal endeavors that I feel show great example of my skills through various programming languages from PHP to Java, JavaScript to Angular. These will showcase the various development skills I have built over time and include links to github repositories.<span class="visible-lg" style="padding-top:15px;"> To the right is a navigation panel for the code snippets below.</span></p>
     </div>
-    <div class="container" style="position:fixed;">
+    <div class="container visible-lg" style="position:fixed;">
         <div class="col-lg-4 col-lg-offset-8">
             <div class="panel panel-default">
                 <div class="panel-heading" style="text-align:center;">
