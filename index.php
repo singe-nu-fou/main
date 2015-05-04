745c653892e9ef924974255c8c78bcaa6b729085
palmeste <palmeste@OAK-2UA3111NTY.amwins.local>
2015-05-04 08:53:21 -0500

    Fleshing out some temporary content.

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-        <title>TEST</title>
         <style>
             
         </style>
