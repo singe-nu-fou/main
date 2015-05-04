8d008699ac37b66886681b9482bd0696993e2186
palmeste <palmeste@OAK-2UA3111NTY.amwins.local>
2015-05-04 09:31:41 -0500

    First stages of shaping the portfolio page. Added an introduction and quick list

--- views/portfolio.php
+++ views/portfolio.php
@@ -8,17 +8,27 @@
     });
 </script>
 <div class="row" style="padding-top:15px;">
-    <div class="col-lg-4 visible-lg">
-        <div class="jumbotron" style="height:175px; margin-bottom:0px;">
-            
+    <div class="col-lg-8" style="height:10000px;">
+        <p style="text-align:justify;">As I am primarily a language agnostic web developer, I use a variety of software languages to attain the results that the current job calls for. Below I will post content that I have created in my personal endeavors that I feel show great example of my skills through various programming languages from PHP to Java, JavaScript to Angular. These will showcase the various development skills I have built over time and include links to github repositories. To the right is a navigation panel for the code snippets below.</p>
+    </div>
+    <div class="container" style="position:fixed;">
+        <div class="col-lg-4 col-lg-offset-8">
+            <div class="panel panel-default">
+                <div class="panel-heading" style="text-align:center;">
+                    Quick Links
+                </div>
+                <ul class="list-group" style="list-style-type:none;">
+                    <a class="list-group-item"><li>Test Link</li></a>
+                    <a class="list-group-item"><li>Test Link</li></a>
+                    <a class="list-group-item"><li>Test Link</li></a>
+                    <a class="list-group-item"><li>Test Link</li></a>
+                    <a class="list-group-item"><li>Test Link</li></a>
+                    <a class="list-group-item"><li>Test Link</li></a>
+                </ul>
+                <div class="panel-footer">
+                    
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="col-lg-8 col-md-12">
-        <p style="text-align:justify;">"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12">
-        <p style="text-align:justify;">"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
     </div>
 </div>
