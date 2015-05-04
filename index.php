f351ea88ce0376d57af6d9e0fecc79f314dab70c
palmeste <palmeste@OAK-2UA3111NTY.amwins.local>
2015-05-04 09:11:43 -0500

    Fleshing out some temporary content.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-        <title></title>
+        <title>Loading...</title>
         <style>
             
         </style>
@@ -15,7 +15,6 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
         <script>
             $(document).ready(function(){
-                $('title').text('Loading...');
                 if(location.hash === ''){
                     location.hash = '#home';
                 }
